b2d077fd16d4ec78a3437a7f2810136f777c6cb6
Valde2609 <valde2609@gmail.com>
2025-09-22 12:46:39 +0200

    Added a new class "MC" That has 2 properties; "LicensePlate" and "Date", and two methods; "Price" and "Vehicle". And two tests that tests the methods

---
+++ Bridge/MC.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bridge;
+
+public class MC
+{
+	/// <summary>
+	/// License plate of the MC And Date of the MC
+	/// </summary>
+	public string LicensePlate { get; set; }
+	public DateTime Date { get; set; }
+
+	/// <summary>
+	/// Method that fetches the price of the MC
+	/// </summary>
+	/// <returns></returns>
+	public double Price()
+	{
+		return 120;
+	}
+	/// <summary>
+	/// Method that fetches the type of vehicle
+	/// </summary>
+	/// <returns></returns>
+	public string Vehicle()
+	{
+		return "MC";
+	}
+
+}
---
+++ BridgeTests/MCTests.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Bridge;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bridge.Tests
+{
+	[TestClass()]
+	public class MCTests
+	{
+		[TestMethod()]
+		public void PriceTest()
+		{
+			//Arrange
+			MC mc = new MC();
+			//Act
+			double result = mc.Price();
+			//Assert
+			Assert.AreEqual(120, result);
+		}
+		[TestMethod()]
+		public void VehicleTest()
+		{
+			//Arrange
+			MC mc = new MC();
+			//Act
+			string result = mc.Vehicle();
+			//Assert
+			Assert.AreEqual("MC", result);
+		}
+	}
+}
