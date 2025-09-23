fc1aefee3e131207b58a8f40ef6bef672af908ec
Valde2609 <valde2609@gmail.com>
2025-09-23 11:29:08 +0200

    Created new OresundBron Ticket Library which builds on the original Bridge DLL; It has two methods OresundPrice and VehicleType

---
+++ OresundBron/OresundBronTicket.cs
@@ -0,0 +1,59 @@
+﻿namespace OresundBron;
+using Bridge;
+
+public class OresundBronTicket
+{
+	/// <summary>
+	/// Method that fetches the price of the oresund ticket based on the type of vehicle and if the vehicle has a brobizz
+	/// Vehicle must be specified as either Car or MC, otherwise an exception is thrown
+	/// </summary>
+	/// <param name="vehicle"></param>
+	/// <returns></returns>
+	/// <exception cref="ArgumentException"></exception>
+	public double OresundPrice(Vehicle vehicle)
+	{
+		double price = 0;
+		if (vehicle is Car)
+		{
+			price = 460;
+			if (vehicle.BroBizz)
+			{
+				price = 178;
+			}
+		}
+		else if (vehicle is MC)
+		{
+			price = 235;
+			if (vehicle.BroBizz)
+			{
+				price = 92; 
+			}
+		}
+		else
+		{
+			throw new ArgumentException("vehicle type must be Car or MC.");
+		}
+		return price;
+	}
+	/// <summary>
+	/// Method that fetches the type of vehicle as either "Oresund Car" or "Oresund MC"
+	/// </summary>
+	/// <param name="vehicle"></param>
+	/// <returns></returns>
+	/// <exception cref="ArgumentException"></exception>
+	public string VehicleType(Vehicle vehicle)
+	{
+		if (vehicle is Car)
+		{
+			return "Oresund Car";
+		}
+		else if (vehicle is MC)
+		{
+			return "Oresund MC";
+		}
+		else
+		{
+			throw new ArgumentException("vehicle type must be Car or MC.");
+		}
+	}
+}
---
+++ OresundBronTests/OresundBronTicketTests.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OresundBron;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Bridge;
+using System.Diagnostics;
+
+namespace OresundBron.Tests
+{
+	[TestClass()]
+	public class OresundBronTicketTests
+	{
+		[TestMethod()]
+		public void CarPriceNoBrobizz()
+		{
+			//Arrange
+			Car car = new Car("12345", false, new DateTime(2024, 5, 30));
+			OresundBronTicket oresundBronTicket = new OresundBronTicket();
+
+			//Act
+			double result = oresundBronTicket.OresundPrice(car);
+
+			//Assert
+			Assert.AreEqual(460, result);
+		}
+		[TestMethod()]
+		public void CarPriceWithBrobizz()
+		{
+			//Arrange
+			Car car = new Car("12345", true, new DateTime(2024, 5, 30));
+			OresundBronTicket oresundBronTicket = new OresundBronTicket();
+
+			//Act
+			double result = oresundBronTicket.OresundPrice(car);
+
+			//Assert
+			Assert.AreEqual(178, result);
+		}
+		[TestMethod()]
+		public void MCPriceNoBrobizz()
+		{
+			//Arrange
+			MC mc = new MC("12345", false, new DateTime(2024, 5, 30));
+			OresundBronTicket oresundBronTicket = new OresundBronTicket();
+
+			//Act
+			double result = oresundBronTicket.OresundPrice(mc);
+
+			//Assert
+			Assert.AreEqual(235, result);
+		}
+		[TestMethod()]
+		public void MCPriceWithBrobizz()
+		{
+			//Arrange
+			MC mc = new MC("12345", true, new DateTime(2024, 5, 30));
+			OresundBronTicket oresundBronTicket = new OresundBronTicket();
+
+			//Act
+			double result = oresundBronTicket.OresundPrice(mc);
+
+			//Assert
+			Assert.AreEqual(92, result);
+		}
+		[TestMethod()]
+		public void VehicleTypeCar()
+		{
+			//Arrange
+			Car car = new Car("12345", false, new DateTime(2024, 5, 30));
+			OresundBronTicket oresundBronTicket = new OresundBronTicket();
+			//Act
+			string result = oresundBronTicket.VehicleType(car);
+			//Assert
+			Assert.AreEqual("Oresund Car", result);
+		}
+		[TestMethod()]
+		public void VehicleTypeMC()
+		{
+			//Arrange
+			MC mc = new MC ("12345", false, new DateTime(2024, 5, 30));
+			OresundBronTicket oresundBronTicket = new OresundBronTicket();
+			//Act
+			string result = oresundBronTicket.VehicleType(mc);
+			//Assert
+			Assert.AreEqual("Oresund MC", result);
+		}
+		
+	}
+}
