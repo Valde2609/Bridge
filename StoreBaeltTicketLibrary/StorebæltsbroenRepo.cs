e9b514fc6da3965e88b6a17c164645d170fa0db6
Valde2609 <valde2609@gmail.com>
2025-09-23 16:03:08 +0200

    Added undone Repo to Storebaelt

---
+++ StoreBaeltTicketLibrary/StorebæltsbroenRepo.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Bridge;
+
+namespace StoreBaeltTicketLibrary;
+
+class StorebæltsbroenRepo
+{
+	/// <summary>
+	/// Static List that holds all the tickets bought for Storebæltsbroen
+	/// </summary>
+	private static List<StorebaeltTicket> _storebæltsbroenTicketList = new List<StorebaeltTicket>();
+
+	/// <summary>
+	/// Method that buys/adds a ticket to the list of tickets
+	/// </summary>
+	/// <param name="ticket"></param>
+	public void BuyTicket(StorebaeltTicket ticket)
+	{
+		_storebæltsbroenTicketList.Add(ticket);
+	}
+	/// <summary>
+	/// Method that fetches all the tickets bought for Storebæltsbroen
+	/// </summary>
+	/// <returns></returns>
+	public List<StorebaeltTicket> GetAllTickets()
+	{
+		return _storebæltsbroenTicketList;
+	}
+	//TODO: Implement FindAllTicketsForVehicle
+	public void FindAllTicketsForVehicle(Vehicle vehicle)
+	{
+		
+		//var matchingVehicles = _storebæltsbroenTicketList
+		//	.Where(v => v.LicensePlate == targetPlate)
+		//	.ToList();
+
+		//return matchingVehicles;
+
+
+
+		//foreach (var v in _storebæltsbroenTicketList)
+		//{
+		//	Console.WriteLine($"{vehicle.VehicleType()} with plate {vehicle.LicensePlate}");
+		//}
+	}
+}
